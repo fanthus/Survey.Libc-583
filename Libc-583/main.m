c443a19f797beea5dda52ed906bfd5f1d65e6749
xiushan.fan <fanxiushan@smartisan.com>
2017-04-24 19:18:44 +0800

    add empty project to manage libc source code.
    Signed-off-by: xiushan.fan <fanxiushan@smartisan.com>

---
+++ Libc-583/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Libc-583
+//
+//  Created by xiushan.fan on 2017/4/24.
+//  Copyright © 2017年 xiushan.fan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
